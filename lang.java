f286292156a67d7bb890d59df7fdd44f7a90e770
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2020-12-31 16:09:48 +0330

    Add files via upload

---
+++ lang.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class lang {
+    public static void main(String[]args) throws IOException {
+            while(true){
+                char chary = (char) System.in.read();
+                System.out.println("The "+chary+" ASCII number is "+((int) chary));
+            }
+    }
+
+}
